f8cab842926650d18489d7e25da28e8c71021b4b
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-21 13:07:33 +1100

    added routes

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<nav>
+	<div>
+		<div>
+			<a href="/"> CardPi </a>
+		</div>
+		<div>
+			<div class="flex flex-row justify-center space-x-4">
+				<a href="/"><button class="btn variant-filled-primary">Home</button></a>
+				<a href="/perfect-strategy"><button class="btn variant-filled-primary">Perfect Strategy</button></a>
+			</div>
+		</div>
+	</div>
+</nav>
