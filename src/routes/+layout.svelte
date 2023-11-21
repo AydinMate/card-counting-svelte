2fd35c7165d62d6b21085d5674811dfe7afe45c2
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-22 09:40:04 +1100

    add responsive checks to answers.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,14 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { Modal, Toast, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
 </script>
 
 <Navbar />
+<Modal />
 <slot />
--- src/routes/perfect-strategy/+page.svelte
+++ src/routes/perfect-strategy/+page.svelte
@@ -1,30 +1,43 @@
 <script lang="ts">
-	import { ProgressRadial } from '@skeletonlabs/skeleton';
-	import { onMount, afterUpdate } from 'svelte';
+	import { ProgressRadial, type ModalSettings } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
 	import axios from 'axios';
 
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+	const modalStore = getModalStore();
+
+	const triggerModal = (question: App.Question) => {
+		const correctAnswer = options.find((option) => option.value === question.answer);
+		const modal: ModalSettings = {
+			type: 'alert',
+			title: 'Incorrect Answer',
+			body: `The dealer's upcard was ${question.dealerUpcard} and the player's total was ${question.playerTotal}. The correct answer was ${correctAnswer?.text}.`,
+			buttonTextCancel: 'Okay'
+		};
+		modalStore.trigger(modal);
+	};
+
 	let question: App.Question;
+
+	let showBackground: boolean = false;
 
 	let options = [
 		{
 			value: 'H',
-			text: 'Hit',
-			isCorrect: false
+			text: 'Hit'
 		},
 		{
 			value: 'S',
-			text: 'Stand',
-			isCorrect: false
+			text: 'Stand'
 		},
 		{
 			value: 'D',
-			text: 'Double/Hit',
-			isCorrect: false
+			text: 'Double/Hit'
 		},
 		{
 			value: 'DS',
-			text: 'Double/Stand',
-			isCorrect: false
+			text: 'Double/Stand'
 		}
 	];
 
@@ -35,31 +48,25 @@
 	};
 
 	const checkAnswer = (answer: string) => {
-		options.forEach(option => {
-			if (option.value === question.answer) {
-				option.isCorrect = true;
-			} else {
-				option.isCorrect = false;
-			}
-		});
-
-		console.log(options)
-		// if (answer === question.answer) {
-		// 	alert('Correct!');
-		// } else {
-		// 	alert('Incorrect!');
-		// }
-		// generateQuestion();
+		console.log(options);
+		if (answer === question.answer) {
+			showBackground = true;
+			setTimeout(() => {
+				showBackground = false;
+			}, 300);
+		} else {
+			triggerModal(question);
+		}
+		generateQuestion();
 	};
 
 	onMount(generateQuestion);
-
-$: {options = [...options]; }
-		
-
 </script>
 
 <div class="text-center">
+	{#if showBackground}
+		<div class="background"></div>
+	{/if}
 	<h1 class="h1">Perfect Strategy</h1>
 	<div class="flex justify-center flex-col mt-[4rem]">
 		<div class="flex-col flex mx-auto">
@@ -77,7 +84,7 @@ $: {options = [...options]; }
 						{#each options as option}
 							<button
 								on:click={() => checkAnswer(option.value)}
-								class="btn {option.isCorrect ? 'variant-filled-success' : 'variant-filled-primary'} m-2">{option.text}</button
+								class="btn variant-filled-primary m-2">{option.text}</button
 							>
 						{/each}
 					</div>
@@ -88,3 +95,18 @@ $: {options = [...options]; }
 		</div>
 	</div>
 </div>
+
+<style>
+	.background {
+		background-color: rgb(0, 161, 0);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+	}
+</style>
